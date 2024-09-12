cd33d54879e91b506f0db50e5c4f40c37766c628
kirisauce <kirisauce@outlook.com>
2024-09-12 10:58:15 +0800

    深色模式

---
+++ composables/theme.ts
@@ -0,0 +1,50 @@
+import { persistentValue } from '~/composables/storage.ts';
+
+function getSystemTheme(): string {
+    if (process.client) {
+        let m = window.matchMedia('(prefers-color-scheme: dark)');
+
+        if (m.matches) {
+            alert('dark!');
+            return 'dark';
+        } else {
+            return 'light';
+        }
+    } else {
+        return 'light';
+    }
+}
+
+function getFinalTheme() {
+    let preference = themePreference.value;
+
+    if (preference == 'system') {
+        return systemTheme.value;
+    } else if (preference == 'dark') {
+        return 'dark';
+    } else if (preference == 'light') {
+        return 'light';
+    }
+}
+
+const systemTheme = ref(getSystemTheme());
+
+export const themePreference = persistentValue('theme.preference', () => 'system');
+export const finalTheme = ref(getFinalTheme());
+
+watch(themePreference, (newValue) => {
+    finalTheme.value = getFinalTheme();
+});
+
+watch(systemTheme, (newValue) => {
+    if (themePreference.value == 'system') {
+        finalTheme.value = newValue;
+    }
+});
+
+if (process.client) {
+    let m = window.matchMedia('(prefers-color-scheme: dark)');
+    m.addEventListener('change', (e) => {
+        systemTheme.value = getSystemTheme();
+    });
+}
--- pages/index.vue
+++ pages/index.vue
@@ -23,6 +23,26 @@ useHead({
         },
     ],
 });
+
+function applyTheme(theme: string) {
+    let classList = document.body.classList;
+
+    if (theme == 'dark') {
+        classList.remove('lightMode');
+        classList.add('darkMode');
+    } else if (theme == 'light') {
+        classList.remove('darkMode');
+        classList.add('lightMode');
+    }
+}
+
+watch(finalTheme, (newTheme) => {
+    applyTheme(newTheme);
+});
+
+if (process.client) {
+    applyTheme(finalTheme.value);
+}
 </script>
 
 <style>
@@ -30,14 +50,18 @@ body {
     width: 100%;
     min-height: 100%;
 
-    background-color: var(--primary-key);
+    background-color: var(--primary-bg);
+
+    transition: --primary-bg 1s, --primary-fg 1s;
 }
 
 .darkMode {
-    --primary-color: #374151;
+    --primary-bg: #374151;
+    --primary-fg: #cccccc;
 }
 
 .lightMode {
-    --primary-color: #000000;
+    --primary-bg: #ffffff;
+    --primary-fg: #123123;
 }
 </style>
